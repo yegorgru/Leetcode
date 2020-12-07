95691c8553509f821980fcf5851ec1e68af585bd
Єгор Грушевий <yegorgru@gmail.com>
2020-12-07 21:36:49 +0200

    Create #29 Divide Two Integers.cpp

---
+++ #29 Divide Two Integers.cpp
@@ -0,0 +1,20 @@
+﻿#include <cmath>
+
+//Divide Two Integers
+
+//https://leetcode.com/problems/divide-two-integers/
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+
+        double d1 = log(fabs(dividend));
+        double d2 = log(fabs(divisor));
+
+        long long res = double(exp(d1 - d2));
+        if ((dividend < 0) ^ (divisor < 0)) res = -res;
+
+        if (res > INT_MAX) return INT_MAX;
+        return res;
+    }
+};
