cf54fb712b0ceab53ff3f5770cf851d8305010d5
Єгор Грушевий <yegorgru@gmail.com>
2020-12-07 20:13:10 +0200

    Create #27 Remove Element.cpp

---
+++ #27 Remove Element.cpp
@@ -0,0 +1,15 @@
+﻿#include <vector>
+#include <algorithm>
+
+//Remove Duplicates from Sorted Array
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val) {
+        auto it = std::remove(nums.begin(), nums.end(), val);
+        nums.erase(it, nums.end());
+        return nums.size();
+    }
+};
